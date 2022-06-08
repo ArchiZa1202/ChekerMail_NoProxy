8dab56e48787efc7fff7eea1f5febad381b5424c
Артур Замотохин <blf_blf_blf3@mail.ru>
2022-06-09 03:02:37 +0500

    start false

---
+++ ChekerMail/Accounts.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChekerMail
+{
+    class Accounts
+    {
+        private Dictionary<string, string> _acc;
+        public string this[string key] 
+        {
+            get => _acc[key];
+            set => _acc[key] = value;
+        }
+        public Accounts(params string [] acc) 
+        {
+            
+        }
+    }
+}
